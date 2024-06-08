0a2894df0f4508e3e1074327958f5fefe1fb06af
mucheru <mucherucaleb7@gmail.com>
2024-06-08 22:04:06 +0300

    Resolve createRoot warning

--- frontend/src/components/BookSearchResults.js
+++ frontend/src/components/BookSearchResults.js
@@ -1,11 +1,10 @@
-// BookSearchResults.js
 import React from 'react';
 import BookItem from './BookItem';
 
 const BookSearchResults = ({ searchResults }) => {
-  // Check if searchResults is undefined or null
-  if (!searchResults) {
-    return null; // Or render a loading indicator or placeholder
+  // Render the component only if there are search results
+  if (!searchResults || searchResults.length === 0) {
+    return null; // Or render a message indicating no search results
   }
 
   return (
